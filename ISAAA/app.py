1b774431ef1ccbb705c5c34ba31185f1e85ef893
1234manucho <onecocktail544@gmail.com>
2025-11-04 21:20:36 +0300

    agritrue backend

--- ISAAA/app.py
+++ ISAAA/app.py
@@ -448,7 +448,7 @@ def dashboard():
     return render_template('dashboard.html')
 # ml analyzer
 @app.route('/ml-analyzer', methods=['GET', 'POST'])
-# @login_required
+@login_required
 def ml_analyzer():
     analysis_result = None
     chart_data = {}
