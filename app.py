2ab01ebc4498b994e54ce00ad1881e066fb5c4a3
Chills <onecocktail544@gmail.com>
2026-06-20 19:58:32 +0300

    trade

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request, redirect, url_for, flash, ses
 from flask_cors import CORS
 from flask_login import LoginManager, login_user, logout_user, login_required, current_user, UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
-
+from flask_migrate import Migrate
 from sqlalchemy import desc
 import os
 import re
@@ -12,11 +12,11 @@ import uuid
 from datetime import datetime, timedelta
 
 import docx
-
+from PyPDF2 import PdfReader
 from PIL import Image
 
 # ------------------- LOCAL IMPORTS -------------------
-from extensions import db
+from extensions import db, migrate
 from models import CommunityNote, Comment, User, DiagnosticResult
 
 # ------------------- FIREBASE -------------------
